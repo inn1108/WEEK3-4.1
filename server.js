33b5fed7fc08fda7f6e2af005732b49a17499fd4
inn1108 <jpei@deakin.edu.au>
2023-03-25 01:51:01 +1100

    message change

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const logger = winston.createLogger({
 
 //
 // If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) NMNMNM`
 //
 if (process.env.NODE_ENV !== 'production') {
   logger.add(new winston.transports.Console({
